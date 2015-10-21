76d3e2a60e755eed78f3a0080e253c5957c4436d
vishvvaw <vizzminzi@gmail.com>
2015-10-21 13:26:41 -0500

    Added Ban Acc and Words

--- run.py
+++ run.py
@@ -16,49 +16,70 @@ api = tweepy.API(auths)
 class listener(StreamListener):
     def on_data(self, raw_data):
         try:
-            retweet(raw_data.split('"id":')[1].split('"id_str":')[0].replace(",", ""))
+            tweet_text = raw_data.lower().split('"text":"')[1].split('","source":"')[0].replace(",", "")
+            screen_name = raw_data.lower().split('"screen_name":"')[1].split('","location"')[0].replace(",", "")
+            tweet_cid = raw_data.split('"id":')[1].split('"id_str":')[0].replace(",", "")
+
+            accs = ['twitter' , 'twittersupport'] # banned account screen name goes in here
+            words = ['hate' , 'derp'] # banned words goes in here
+
+            if not any(acc in screen_name.lower() for acc in accs):
+                if not any(word in tweet_text.lower() for word in words):
+                    # call what u want to do here
+                    #fav(tweet_cid)
+                    #retweet(tweet_cid)
             return True
-        except Exception:
+        except Exception as e:
+            print(str(e)) # prints the error msg, if u dont want it comment it out
             pass
 			
 
     def on_error(self, status_code):
         try:
             print( "error" + status_code)
-        except Exception:
+        except Exception as e:
+            print(str(e))
             pass
 			
 
-def retweet(myid):
+def retweet(tweet_cid):
     try:
-        api.retweet(myid)
-    except Exception:
+        api.retweet(tweet_cid)
+    except Exception as e:
+        print(str(e))
         pass
 
 
-def fav(myid):
+def fav(tweet_cid):
     try:
-        api.create_favorite(myid)
-    except Exception:
+        api.create_favorite(tweet_cid)
+    except Exception as e:
+        print(str(e))
         pass
 
 
-def unfav(myid):
+def unfav(tweet_cid):
     try:
-        api.destroy_favorite(myid)
-    except Exception:
+        api.destroy_favorite(tweet_cid)
+    except Exception as e:
+        print(str(e))
         pass
 
 
 def tweet(myinput):
     try:
         api.update_status(myinput)
-    except Exception:
+    except Exception as e:
+        print(str(e))
         pass
 
 
 try:
     twt = Stream(auths, listener())
-    twt.filter(track=["#lka","SriLanka","@Jothipala_Bot","@SiripalaBot"])
-except Exception:
+
+    track_words = ["#lka","SriLanka","@Jothipala_Bot","@SiripalaBot"]
+    follow_acc = ['2312312' , '1234332'] # all username converted to user ids
+    twt.filter(track= track_words , follow = follow_acc)
+except Exception as e:
+    print(str(e))
     pass
