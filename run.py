ab53d23f66dd8726eed3c15e5f7f55508388c18a
vishwa <vizzminzi@gmail.com>
2016-05-14 21:25:52 -0500

    Huge Update
    Added whitelist words and accounts, if the code doesn't work, checkout
    the old code.

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-#!/usr/local/bin/python3.3
-
+#!/usr/local/bin/python2.7
 import tweepy
 from tweepy.auth import OAuthHandler
 from tweepy.streaming import StreamListener, Stream
+from tweepy.auth import OAuthHandler
 
 ckey = ''
 csecret = ''
@@ -16,67 +16,66 @@ api = tweepy.API(auths)
 class listener(StreamListener):
     def on_data(self, raw_data):
         try:
-            tweet_text = raw_data.lower().split('"text":"')[1].split('","source":"')[0].replace(",", "")
-            screen_name = raw_data.lower().split('"screen_name":"')[1].split('","location"')[0].replace(",", "")
-            tweet_cid = raw_data.split('"id":')[1].split('"id_str":')[0].replace(",", "")
-
-            accs = ['twitter' , 'twittersupport'] # banned account screen name goes in here
-            words = ['hate' , 'derp'] # banned words goes in here
-
-            if not any(acc in screen_name.lower() for acc in accs):
-                if not any(word in tweet_text.lower() for word in words):
-                	# call what u want to do here
-                    #fav(tweet_cid)
-                	#retweet(tweet_cid)
-            		return True
-            	
+            tweet_text = raw_data.lower().split('"text":"')[1].split('","source":"')[0].replace(",", "") #tweet's text
+            screen_name = raw_data.lower().split('"screen_name":"')[1].split('","location"')[0].replace(",", "") #tweet's authors screen name
+            tweet_sid = raw_data.split('"id":')[1].split('"id_str":')[0].replace(",", "") #tweet's id
+			
+			#whitelist handles and words goes in here
+            whitelist_acc = [' ', ' ']
+            whitelist_words = [' ', ' ']
+			
+            #banned handles and words goes in here
+            banned_accs =  [' ' ,' ']            
+            banned_words = [' ' ,' ']
+			
+            if not any(a_acc == screen_name.lower() for a_acc in whitelist_acc):
+                if not any(acc == screen_name.lower() for acc in banned_accs):
+                    if not any(a_wrds in screen_name.lower() for a_wrds in whitelist_words):
+                        if not any(word in tweet_text.lower() for word in banned_words):
+                            #call what u want to do here
+                            #for example :
+                            #fav(tweet_sid)
+                            #retweet(tweet_sid)
+                    else:
+                        #call what u want to do here
+                        #for example :
+                        #fav(tweet_sid)
+                        #retweet(tweet_sid)
+            else:
+                #call what u want to do here
+                #for example :
+                #fav(tweet_sid)
+                #retweet(tweet_sid)
+            return True
         except Exception as e:
             print(str(e)) # prints the error msg, if u dont want it comment it out
             pass
-			
 
     def on_error(self, status_code):
         try:
-            print( "error" + status_code)
+            print( "error " + status_code)
         except Exception as e:
             print(str(e))
-            pass
-			
+        pass
 
-def retweet(tweet_cid):
+
+def retweet(tweet_sid):
     try:
-        api.retweet(tweet_cid)
+        api.retweet(tweet_sid)
     except Exception as e:
         print(str(e))
         pass
 
 
-def fav(tweet_cid):
+def fav(tweet_sid):
     try:
-        api.create_favorite(tweet_cid)
+        api.create_favorite(tweet_sid)
     except Exception as e:
         print(str(e))
         pass
 
-
-def unfav(tweet_cid):
-    try:
-        api.destroy_favorite(tweet_cid)
-    except Exception as e:
-        print(str(e))
-        pass
-
-
-def tweet(myinput):
-    try:
-        api.update_status(status=myinput)
-    except Exception as e:
-        print(str(e))
-        pass
-
-
-track_words = ["#lka","SriLanka","@Jothipala_Bot"]
-follow_acc = ['2312312' , '1234332'] # all username converted to user ids
+track_words = [" "," "," "]
+follow_acc = ['20536157',"20573247"] #retweets every tweet from this accounts, handles converted to ids 
 
 print("Running...")
 try:
